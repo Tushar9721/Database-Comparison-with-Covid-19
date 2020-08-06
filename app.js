eb4ba1b2f254d102aaa739353cb466a8f2f49640
Tushar Garg <garg31@uwindsor.ca>
2020-08-05 23:05:56 -0400

    Milestone 2

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const https = require("https");
+const mysql = require("mysql");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -10,31 +12,63 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+var mysqlConnection = mysql.createConnection({
+  host:"localhost",
+  user: "root",
+  password:"root",
+  database: "database",
+  insecureAuth: true
+});
+
+mysqlConnection.connect((err)=>{
+
+  if(!err){
+    console.log("Done");
+  }
+  else{
+    console.log(err);
+  }
+
+});
+// //connecting with mongo
+// mongoose.connect("mongodb://localhost:27017/covidDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// //creating schema
+// const caseSChema = {
+//   countryName: String,
+//   date: String,
+//   recentCases: String,
+//   lastCases: String,
+//   recetnDeaths: String,
+//   lastDeath: String,
+// };
+
+// const Case = mongoose.model("Case", caseSChema);
+
 //getting the home route..
 app.get("/", function (req, res) {
-  var countryCodes = [];
-  https
-    .get(
-      "https://covid.ourworldindata.org/data/owid-covid-data.json",
-      (resp) => {
-        let data = "";
 
-        // A chunk of data has been recieved.
-        resp.on("data", function (chunk) {
-          data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        resp.on("end", function () {
-          //parsing the data
-          const covidData = JSON.parse(data);
-        });
-      }
-    )
-    .on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
   res.render("comparison");
+
+  // const covid_case = new Case({
+  //   countryName: "India",
+  //   date: "202020",
+  //   recentCases: "20202",
+  //   lastCases: "1202",
+  //   recetnDeaths: "12",
+  //   lastDeath: "23",
+  // });
+
+  // covid_case.save(function (err) {
+  //   if (!err) {
+  //     console.log("DOne");
+  //     res.render("comparison");
+  //   }
+  // });
+ 
 });
 
 //getting the cases request...
@@ -68,10 +102,21 @@ app.get("/cases", function (req, res) {
           //parsing the data
           const covidData = JSON.parse(data);
 
-          //getting the country code
-          for (let i = 0; i < Object.keys(covidData).length; i++) {
-            newCountryCodes.push(Object.keys(covidData));
-          }
+          // for (let i = 0; i < Object.keys(covidData).length; i++) {
+          //   newCountryCodes.push(Object.keys(covidData));
+          // }
+          console.log(Object.values(covidData)[0].location);
+          console.log(
+            Object.values(covidData)[0].data[
+              Object.values(covidData)[0].data.length - 1
+            ]
+          );
+          console.log(
+            Object.values(covidData)[0].data[
+              Object.values(covidData)[0].data.length - 2
+            ]
+          );
+
           countryData = covidData.CAN;
           var lengthDate = countryData.data.length;
 
@@ -96,7 +141,6 @@ app.get("/cases", function (req, res) {
 
           res.render("cases", {
             countryTitle: countryName,
-            //newCountryCodes: newCountryCodes[0],
             todayDate: todayDate,
             yesterDayDate: yesterdayDate,
             todayTotalDeath: todayTotalDeaths,
@@ -114,11 +158,6 @@ app.get("/cases", function (req, res) {
     .on("error", (err) => {
       console.log("Error: " + err.message);
     });
-  // console.log(countryName);
-});
-
-app.post("/case", function (req, res) {
- 
 });
 
 let port = process.env.PORT;
--- public/script/main.js
+++ public/script/main.js
@@ -1,25 +1,19 @@
 //getting the values from thml file
 //getting the value..
 var readMongo = parseInt(document.getElementById("readM").innerHTML);
+var insertMongo = parseInt(document.getElementById("insertM").innerHTML);
 var upadMongo = parseInt(document.getElementById("upadM").innerHTML);
 var delMongo = parseInt(document.getElementById("delM").innerHTML);
 var readSql = parseInt(document.getElementById("readS").innerHTML);
 var upadSql = parseInt(document.getElementById("upadS").innerHTML);
 var delSql = parseInt(document.getElementById("delS").innerHTML);
-
-
-function myFunction() {
-  var code = document.getElementById("myCountryCodes").value;
-  document.getElementById("custId").innerHTML = code;
-  console.log(code);
-  }
+var insertSql = parseInt(document.getElementById("insertS").innerHTML);
 
 window.onload = function () {
   var chart = new CanvasJS.Chart("chartContainer", {
     animationEnabled: true,
     theme: "light3",
     exportEnabled: true,
-
     axisY: {
       includeZero: false,
     },
@@ -32,16 +26,20 @@ window.onload = function () {
         indexLabelFontSize: 16,
         dataPoints: [
           {
+            label: "Inserting",
+            y: insertMongo,
+          },
+          {
+            label: "Reading",
+            y: readMongo,
+          },
+          {
             label: "Updating",
             y: upadMongo,
           },
           {
             label: "Deletion",
             y: delMongo,
-          },
-          {
-            label: "Reading",
-            y: readMongo,
           },
         ],
       },
@@ -53,17 +51,21 @@ window.onload = function () {
         indexLabelFontSize: 6,
         dataPoints: [
           {
+            label: "Inserting",
+            y: insertSql,
+          },
+          {
+            label: "Reading",
+            y: readSql,
+          },
+          {
             label: "Updating",
             y: upadSql,
           },
           {
             label: "Deletion",
             y: delSql,
-          },
-          {
-            label: "Reading",
-            y: readSql,
-          },
+          }, 
         ],
       },
     ],
